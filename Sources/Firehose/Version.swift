93911885d1eca3652d83ff45d61cb5c7211afb5c
harrisonpage <harrison.page@harrison.page>
2026-09-18 15:59:28 -0700

    add Version.swift to .gitignore

--- Sources/Firehose/Version.swift
+++
@@ -1,6 +0,0 @@
-// Overwritten by build.sh with the real git hash and build date.
-enum Version {
-    static let version = "1.0.0"
-    static let build = "b791c308"
-    static let date = "Sep 4, 2026"
-}
